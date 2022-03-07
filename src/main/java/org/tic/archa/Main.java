59da7ff31014a33446a2bd8c4cf5bcc6cd751541
gopal-isaac <gopal.issac@gmail.com>
2022-03-07 17:05:30 +0800

    uml diagram

--- src/main/java/org/tic/archa/Main.java
+++ src/main/java/org/tic/archa/Main.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String testcase = "1";
-        String inputFile = "Assignment1Testcases/testcase"+testcase+"/TitlesInput.txt";
-        String outputFile = "Assignment1Testcases/testcase"+testcase+"/TitlesOutputA.txt";
+        String inputFile = args[0] + "/TitlesInput.txt";
+        String outputFile = args[0] +"/TitlesOutputA.txt";
 
         List<String> listOfTitles = FileReader.readFile(inputFile);
         listOfTitles = CircularShifter.Shift(listOfTitles);
--- src/main/java/org/tic/archb/Main.java
+++ src/main/java/org/tic/archb/Main.java
@@ -3,8 +3,8 @@ package org.tic.archb;
 public class Main {
     public static void main(String[] args) {
         String testcase = "1";
-        String inputFile = "Assignment1Testcases/testcase"+testcase+"/TitlesInput.txt";
-        String outputFile = "Assignment1Testcases/testcase"+testcase+"/TitlesOutputB.txt";
+        String inputFile = args[0] + "/TitlesInput.txt";
+        String outputFile = args[0] +"/TitlesOutputA.txt";
         Master abstractDateType = new Master(inputFile,outputFile);
         abstractDateType.runProgram();
     }
